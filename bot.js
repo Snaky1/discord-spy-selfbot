0007c28eabee7f6d430f79a7c6cb9fed5508ef73
ovinu <96349596+ovivvvu@users.noreply.github.com>
2022-07-21 18:24:32 +0300

    поддержка эмбедов(спс посреади)

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 // Импортируем всё что нам нужно
-const { Client, WebhookClient, MessageEmbed } = require('discord.js-selfbot-v13');
-const client = new Client({checkUpdate: false});
+const {Client, WebhookClient, MessageEmbed} = require('discord.js-selfbot-v13')
+const client = new Client({checkUpdate: false})
 const config = require('./config.json')
 
 // Создаём вебхук
-const webhook = new WebhookClient({ url: config.logwebhook}, { allowedMentions: { parse: [], users: [], roles: [] }});
+const webhook = new WebhookClient({url: config.logwebhook}, {allowedMentions: {parse: [], users: [], roles: []}})
 
 // Ловим ошибки чтобы бот не падал
 process.on('unhandledRejection', error => {
-	console.error('Фатальная ошибка:', error);
+    console.error('Фатальная ошибка:', error)
 });
 
 client.on('error', error => {
@@ -17,35 +17,40 @@ client.on('error', error => {
 
 // Заходим в аккаунт
 client.on('ready', async () => {
-  console.log(`Вы зашли в аккаунт ${client.user.username}#${client.user.discriminator}(${client.user.id})`);
+    console.log(`Вы зашли в аккаунт ${client.user.username}#${client.user.discriminator}(${client.user.id})`)
 })
 
 client.on('messageCreate', async (msg) => {
+    if (msg.guildId !== config.guild) return
 
-if(msg.guildId !== config.guild) return; // Не выполняем код ниже, если айди сервера с которого пришло сообщение не равен айди указанному в конфиге
-
-if(config.returnbots) { 
-    if(msg.author.bot) return;
-    if (msg.webhookId) return;
-} // Если config.returnbots равен true - игнорировать ботов и другие вебхуки
+    if (config.returnbots) if (msg.webhookId) return
+    if (!config.returnbots) {
+        if (msg.author.bot) return
+        if (msg.webhookId) return
+    }
 
 // Создаём эмбед
-let emb = new MessageEmbed()
-    .setAuthor({iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})`, inline: true})
-    .setDescription(msg.content)
-    .setColor('#534be4')
-    .setFooter({text: `Selfbot by @rxiteel || ovinu#0135`})
-    
-    if (msg.attachments.size > 0) {
-        emb.setImage(msg.attachments.map(a=>a.url)[0])
-    } // Если в сообщение есть вложение - добавить его в эмбед
-    
+    let emb = new MessageEmbed()
+        .setAuthor({iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})`, inline: true})
+        .setDescription(msg.content)
+        .setColor('#534be4')
+        .setFooter({text: `Selfbot by @rxiteel || ovinu#0135`})
+
+    if (msg.attachments.size > 0) emb.setImage(msg.attachments.map(a => a.url)[0]) // Если в сообщение есть вложение - добавить его в эмбед
+    if (msg.embeds.length > 0) {
+        for (let i = 0; i <= msg.embeds.length; i++) {
+            const embed = msg.embeds[i]
+            await webhook.send({
+                embeds: [embed]
+            })
+        }
+    }
     // Отправляем сообщение вебхуком
-    webhook.send({
+    await webhook.send({
         embeds: [emb],
         username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
-        avatarURL: `${client.guilds.cache.get(config.guild).iconURL()}`
+        avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/imgv/ixJomm_no-avatar-png-circle-transparent-png/"}`
     })
 })
 
-client.login(config.token);
+client.login(config.token).then(_ => console.log("Token is working")).catch(_ => console.log("Token is not working"));
