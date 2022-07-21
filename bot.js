2a3b76df9260797f5c970e232f08a575405e348c
ovinu <96349596+ovivvvu@users.noreply.github.com>
2022-07-22 00:50:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-// Импортируем всё что нам нужно
 const { Client, WebhookClient, MessageEmbed } = require('discord.js-selfbot-v13');
 const client = new Client({checkUpdate: false});
 const config = require('./config.json')
@@ -22,45 +21,39 @@ client.on('ready', async () => {
 
 client.on('messageCreate', async (msg) => {
 
-if(msg.guildId !== config.guild) return; // Не выполняем код ниже, если айди сервера с которого пришло сообщение не равен айди указанному в конфиге
+    if(msg.guildId !== config.guild) return; // Не выполняем код ниже, если айди сервера с которого пришло сообщение не равен айди указанному в конфиге
 
-if(config.nobots) { 
-    if(msg.author.bot) return;
-} // Если config.nobots равен true - игнорировать ботов
-if(config.nowebhooks) { 
-    if(msg.webhookId) return;
-} // Если config.nowebhooks равен true - игнорировать вебхуки
+    if(config.nobots && msg.author.bot) return;    // Если config.nobots равен true - игнорировать вебхуки
+    if(config.nowebhooks && msg.webhookId) return; // Если config.nowebhooks равен true - игнорировать вебхуки
+    //зан иди нахуй
 
-// Создаём эмбед
-let emb = new MessageEmbed()
-    .setAuthor({iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})`, inline: true})
-    .setDescription(msg.content)
-    .setColor('#534be4')
-    .setFooter({text: `Selfbot by @rxiteel || ovinu#0135`})
-    
-    if (msg.attachments.size > 0) {
-        emb.setImage(msg.attachments.map(a=>a.url)[0])
-    } // Если в сообщении есть вложение - добавить его в эмбед
-    
-    if (msg.embeds.length > 0) {// Если в сообщении есть эмбеды
-        for (let i = 0; i <= msg.embeds.length; i++) {
-            const embed = msg.embeds[i]
-            await webhook.send({
-                content: `Бот (${msg.author.tag} (${msg.author.id})) отправил сообщение с эмбедом\n${msg.content}`,
-		embeds: [embed],
- 		username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
-		avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/imgv/ixJomm_no-avatar-png-circle-transparent-png/"}`
-            })
+    // Создаём эмбед
+    let emb = new MessageEmbed()
+        .setAuthor({iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})`, inline: true})
+        .setDescription(msg.content)
+        .setColor('#534be4')
+        .setFooter({text: `Selfbot by @rxiteel || ovinu#0135`})
+        
+        if (msg.attachments.size > 0) emb.setImage(msg.attachments.map(a=>a.url)[0]) // Если в сообщении есть вложение - добавить его в эмбед
+        
+        if (msg.embeds.length > 0) {// Если в сообщении есть эмбеды
+            for (let i = 0; i <= msg.embeds.length; i++) {
+                const embed = msg.embeds[i]
+                await webhook.send({
+                    content: `Бот (${msg.author.tag} (${msg.author.id})) отправил сообщение с эмбедом\n${msg.content}`,
+            		embeds: [embed],
+             		username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
+            		avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/imgv/ixJomm_no-avatar-png-circle-transparent-png/"}`
+                })
+            }
+        } 
+
+        if(msg.reference) { // Если сообщение реплай(или закрепление, но на это забейте оно не работает)
+            const repliedTo = await msg.fetchReference()
+            emb.addField(`> В ответ пользователю ${repliedTo.author.username}#${repliedTo.author.discriminator}`,`\`${repliedTo}\``)
         }
-    } 
 
-    if(msg.reference) { // Если сообщение реплай(или закрепление, но на это забейте оно не работает)
-        const repliedTo = await msg.fetchReference()
-        emb.addField(`> В ответ пользователю ${repliedTo.author.username}#${repliedTo.author.discriminator}`,`\`${repliedTo}\``)
-    }
-
-
-    // Отправляем сообщение вебхуком
+        // Отправляем сообщение вебхуком
     await webhook.send({
         embeds: [emb],
         username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
