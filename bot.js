2b1b94e41101d453c50c711b5f991a1f2ebcdcad
ovinu <96349596+ovivvvu@users.noreply.github.com>
2022-07-23 20:00:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,85 +28,44 @@ client.on('messageCreate', async (msg) => {
     // зан иди нахуй
 
     client.on('messageUpdate', async (oldMessage, newMessage) => { // мне лень дальше писать комменты
-        if(msg.author.bot || msg.webhookId) return; // пришлось извините
-    
+        if (msg.author.bot || msg.webhookId) return; // пришлось извините
+
         if (oldMessage.content === newMessage.content) return;
-    
-            const original = oldMessage.content.slice(0, 1950) + (oldMessage.content.length > 1950 ? '...' : '')
-            const edited = newMessage.content.slice(0, 1950) + (newMessage.content.length > 1950 ? '...' : '')
-    
-            var embed = new MessageEmbed()
-            .setAuthor({ iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})`})
+
+        const original = oldMessage.content.slice(0, 1950) + (oldMessage.content.length > 1950 ? '...' : '')
+        const edited = newMessage.content.slice(0, 1950) + (newMessage.content.length > 1950 ? '...' : '')
+
+        var embed = new MessageEmbed()
+            .setAuthor({ iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})` })
             .setTimestamp()
             .setColor('GREEN')
             .addFields(
-                {name: 'Старое:',value: original},
-                {name: 'Новое:', value: edited});
-                await webhook.send({
-                    content: `Юзер (${msg.author.tag} (${msg.author.id})) отредактировал сообщение`,
-                    embeds: [embed],
-                    username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
-                    avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"}`
-                })
+                { name: 'Старое:', value: original },
+                { name: 'Новое:', value: edited });
+        await webhook.send({
+            content: `Юзер (${msg.author.tag} (${msg.author.id})) отредактировал сообщение`,
+            embeds: [embed],
+            username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
+            avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"}`
+        })
     })
 
     // Создаём эмбед
-    let emb = new MessageEmbed()
-        .setAuthor({ iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})`})
+    const emb = new MessageEmbed()
+        .setAuthor({ iconURL: msg.author.avatarURL(), name: `${msg.author.tag} (${msg.author.id})` })
         .setDescription(msg.content)
         .setColor('#534be4')
         .setFooter({ text: `Selfbot by @rxiteel || ovinu#0135` })
 
-     
-        let embedphoto = new MessageEmbed()
-        .setAuthor({
-            iconURL: msg.author.avatarURL(),
-            name: msg.author.tag,
-            inline: true
-        })
-        .setDescription("Все остальные картинки из сообщения")
-        .setColor('#534be4')
-        .setFooter({text: `Selfbot by @rxiteel || ovinu#0135`})
-
-        if (msg.attachments) {
-            // emb.setImage(msg.attachments.map(a => a.url)[0])
-            if (msg.attachments.size > 1) {
-                let attachments_string = ""
-                let attachments_f_desc = ""
-                for (let i = 1; i <= msg.attachments.size; i++) {
-                    try {
-                        // await webhook.send({content: "test", files: [msg.attachments.map(m => m.url)][i]})
-                        attachments_string += `${(msg.attachments.map(m => m.url)[i] !== undefined) ? msg.attachments.map(m => m.url)[i] + '\n' : ''}`
-                    } catch (err) {
-                        console.log(err)
-                    }
-                }
-                if (attachments_string !== '') {
-                    if (attachments_string.split('\n').length === 1) {
-                        embedphoto.setImage(attachments_string)
-                    } else {
-                        let index = attachments_string.split('\n').shift()
-                        emb.setImage(index)
-                        for (let attach of attachments_string.split('\n')) {
-                            attachments_f_desc += `${attach}\n`
-                        }
-                        embedphoto.setDescription(attachments_f_desc)
-                    }
-    
-                } else {
-                }
-            } else embedphoto.setImage(msg.attachments.map(a => a.url)[0])
-        }
-    // Если в сообщении есть вложение - добавить его в эмбед
 
     if (msg.embeds.length > 0 && msg.author.bot) {// Если в сообщении есть эмбеды
         for (let i = 0; i <= msg.embeds.length; i++) {
-            const embed = msg.embeds[i]
-                await webhook.send({
-                    content: `Бот (${msg.author.tag} (${msg.author.id})) отправил сообщение с эмбедом\n${msg.content}`,
-                    embeds: [embed],
-                    username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
-                    avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"}`
+            const embed2 = msg.embeds[i]
+            await webhook.send({
+                content: `Бот (${msg.author.tag} (${msg.author.id})) отправил сообщение с эмбедом\n${msg.content}`,
+                embeds: [embed2],
+                username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
+                avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"}`
             })
         }
     }
@@ -116,10 +75,39 @@ client.on('messageCreate', async (msg) => {
         emb.addField(`> В ответ пользователю ${repliedTo.author.username}#${repliedTo.author.discriminator}`, `\`${repliedTo}\``)
     }
 
+    if (msg.attachments) {
+        // emb.setImage(msg.attachments.map(a => a.url)[0])
+        if (msg.attachments.size > 1) {
+            let attachments_string = ""
+            let attachments_f_desc = ""
+            for (let i = 1; i <= msg.attachments.size; i++) {
+                try {
+                    // await webhook.send({content: "test", files: [msg.attachments.map(m => m.url)][i]})
+                    attachments_string += `${(msg.attachments.map(m => m.url)[i] !== undefined) ? msg.attachments.map(m => m.url)[i] + '\n' : ''}`
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+            if (attachments_string !== '') {
+                if (attachments_string.split('\n').length === 1) {
+                    emb.setImage(attachments_string)
+                } else {
+                    let index = attachments_string.split('\n').shift()
+                    emb.setImage(index)
+                    for (let attach of attachments_string.split('\n')) {
+                        attachments_f_desc += `${attach}\n`
+                    }
+                    emb.addField(`Изображения`, attachments_f_desc)
+                }
 
-    // Отправляем сообщение вебхуком
-    webhook.send({
-        embeds: [emb, embedphoto],
+            } else {
+            }
+        } else emb.setImage(msg.attachments.map(a => a.url)[0])
+    }
+
+
+    webhook.send({ // Отправляем сообщение вебхуком
+        embeds: [emb],
         username: `${client.guilds.cache.get(config.guild).name} / #${msg.channel.name}`,
         avatarURL: `${(client.guilds.cache.get(config.guild).iconURL() !== null) ? client.guilds.cache.get(config.guild).iconURL() : "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"}`
     })
